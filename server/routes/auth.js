28d1e6c98360863382ce1b7a928403101b78f06e
Jon Baclig <jon@baclig.com>
2019-10-17 22:43:54 -0700

    add JWT auth to user registration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,21 +5,25 @@ const jwtConfig = require('../config/jwtConfig');
 
 const router = express.Router();
 
-router.post('/register',
-  passport.authenticate('register', { session: false }),
-  function(request, response) {
-    console.log('register successful');
-    response.redirect('/');
-  }
-);
+router.post('/register', (request, response, next) => {
+  passport.authenticate('register', { session: false }, (err, user, info) => {
+    if(err || !user) {
+      return response.status(400).json({
+        message: 'Somthing went wrong',
+        user: user
+      });
+    }
 
-// router.post('/login', 
-//   passport.authenticate('login', { session: false }),
-//   function(request, response) {
-//     console.log('login successful');
-//     response.redirect('/');
-//   }
-// );
+    request.login(user, { session: false }, (err) => {
+      if(err) {
+        response.send(err);
+      }
+
+      const token = jwt.sign(user, jwtConfig.secret);
+      return response.json({ user, token });
+    });
+  })(request, response, next);
+});
 
 router.post('/login', (request, response, next) => {
   passport.authenticate('login', { session: false }, (err, user, info) => {
